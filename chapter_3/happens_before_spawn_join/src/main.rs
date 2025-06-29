cbd1ea5b3d618b470e59a50f79c5d662522c1587
YawKar <yawkarpov@gmail.com>
2025-06-29 15:42:24 +0300

    happens-before

---
+++ chapter_3/happens_before_spawn_join/src/main.rs
@@ -0,0 +1,19 @@
+use std::{
+    sync::atomic::{AtomicI32, Ordering},
+    thread,
+};
+
+static X: AtomicI32 = AtomicI32::new(0);
+
+fn main() {
+    X.store(1, Ordering::Relaxed);
+    let t = thread::spawn(f);
+    X.store(2, Ordering::Relaxed);
+    t.join().unwrap();
+    X.store(3, Ordering::Relaxed);
+}
+
+fn f() {
+    let x = X.load(Ordering::Relaxed);
+    assert!(x == 1 || x == 2);
+}
